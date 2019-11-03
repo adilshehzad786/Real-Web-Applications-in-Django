0fb0a729dee3b5f38890fb899df6c03aa98cc0db
Adil Shehzad <53600644+adilshehzad786@users.noreply.github.com>
2019-11-03 18:57:27 +0500

    Add files via upload

---
+++ RateMyProfessor/review/urls.py
@@ -0,0 +1,52 @@
+"""review URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth.views import LoginView,LogoutView
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path,include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('secret_admin_panel/', admin.site.urls),
+    path('register/',user_views.register,name='register'),
+    path('profile/',user_views.profile,name='profile'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('',include('djangoproject.urls')),
+
+    path('password_reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>',auth_views.PasswordResetView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password_reset_done/',auth_views.PasswordResetView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+
+
+
+    
+
+
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
